761281d8004f9e16e90a5e7fb1bc82841f2f1099
Esteban1051111 <juan.3801911657@ucaldas.edu.co>
2023-11-29 19:22:25 -0500

    Validacion campos registro

--- public/js/validarCampos.js
+++ public/js/validarCampos.js
@@ -1,50 +1,69 @@
+function validarCampos() {
+    var nombre, documento, direccion, telefono, correo, expresion_correo, programa, usuario, expresion_solo_texto;
 
-    async function validarCampos() {
-        // Resto del código de validación...
+    nombre = document.getElementById("nombre").value;
+    documento = document.getElementById("documento").value;
+    direccion = document.getElementById("direccion").value;
+    telefono = document.getElementById("telefono").value;
+    correo = document.getElementById("correo").value;
+    programa = document.getElementById("programa").value;
+    usuario = document.getElementById("programa").value;
+    expresion_correo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    expresion_solo_texto = /^[A-Za-zÁáÉéÍíÓóÚúñÑ\s]+$/;
+    var contrasena = document.getElementById("contrasena").value;
+    var confirm_contrasena = document.getElementById("confirm_contrasena").value;
 
-        // Validar contraseñas
-        if (!validarContrasena()) {
-            return false;
-        }
-
-        try {
-            const response = await fetch('/usuarios/crear', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    nombre: document.getElementById('nombre').value,
-                    documento: document.getElementById('documento').value,
-                    telefono: document.getElementById('telefono').value,
-                    correo: document.getElementById('correo').value,
-                    usuario: document.getElementById('usuario').value,
-                    contrasena: document.getElementById('contrasena').value,
-                    confirm_contrasena: document.getElementById('confirm_contrasena').value,
-                    rol: document.getElementById('rol').value,
-                }),
-            });
-
-            const data = await response.json();
-
-            if (data.success) {
-                // Limpiar campos del formulario si es necesario
-                document.getElementById('nombre').value = '';
-                document.getElementById('documento').value = '';
-                // Otros campos...
-
-                // Mostrar mensaje de éxito
-                document.getElementById('success').innerText = data.message;
-            } else {
-                // Mostrar mensaje de error en la página
-                document.getElementById('error').innerText = data.message;
-            }
-
-        } catch (error) {
-            console.error('Error al enviar el formulario:', error);
-            alert('Error al enviar el formulario.');
-        }
-
-        return false; // Evitar que el formulario se envíe tradicionalmente
+    if (nombre === "" || documento === "" || direccion === "" || telefono === "" || correo === "") {
+        alert("debe rellenar todos los campos")
+        return false;
     }
+    else if (!expresion_solo_texto.test(nombre)) {
+        alert("Ingrese solo valores de texto")
+        return false;
+    }
+    else if (documento.length >= 12) {
+        alert("el documento es muy largo")
+        return false;
+    }
+    else if (direccion.length >= 60) {
+        alert("la direccion es muy larga ")
+        return false;
+    }
+    else if (telefono.length >= 20) {
+        alert("el telefono es muy largo")
+        return false;
+    }
+    else if (isNaN(telefono)) {
+        alert("ingrese un valor numerico en el telefono")
+        return false;
+    }
+    else if (isNaN(documento)) {
+        alert("ingrese un valor numerico en el documento")
+        return false;
+    }
+    else if (!expresion_correo.test(correo)) {
+        alert("ingrese un correo valido")
+        return false;
+    }
+    else if (!expresion_solo_texto.test(programa)) {
+        alert("Ingrese solo valores de texto")
+        return false;
+    }
+    else if (!expresion_solo_texto.test(usuario)) {
+        alert("Ingrese solo valores de texto")
+        return false;
+    }
+
+    else if (contrasena == confirm_contrasena) {
+        // Mostrar mensaje indicando que las contraseñas coinciden
+        document.getElementById("error").innerHTML = "Las contraseñas coinciden";
+        document.getElementById("error").style.color = "green";
+    } else {
+        // Mostrar mensaje de error indicando que las contraseñas no coinciden
+        document.getElementById("error").innerHTML = "Las contraseñas no coinciden";
+        document.getElementById("error").style.color = "red";
+    }
+
+
+}
 
