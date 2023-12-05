9bfd4fa85fbec1981cb2f215271e1587ccdd4030
Esteban1051111 <juan.3801911657@ucaldas.edu.co>
2023-12-04 23:04:27 -0500

    vista usuarios

--- config/database.js
+++ config/database.js
@@ -3,7 +3,9 @@ import postgres from 'postgres';
 const sql = postgres({
     database: 'AppUcaldas',
     user: 'postgres',
-    password: '1124'
+    password: 'admin'
 });// will use psql environment variables
 
 export default sql;
+
+
